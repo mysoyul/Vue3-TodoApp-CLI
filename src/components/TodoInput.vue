9cedf621f2ca67b475039c73650cd2d6f20d8c88
mysoyul <vega2k_@naver.com>
2023-08-10 15:45:49 +0900

    TodoInput update

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,7 +1,9 @@
 <template>
-    <div>
-        <input type="text" :value="newTodoItem" @input="handleInput">
-        <button @click="addTodo">추가</button>
+    <div class="inputBox shadow">
+        <input type="text" :value="newTodoItem" @input="handleInput" @keyup.enter="addTodo">
+        <span class="addContainer" @click="addTodo">
+            <i class="fas fa-plus addBtn"></i>
+        </span>
     </div>
 </template>
 
@@ -35,28 +37,31 @@ const clearInput = () => {
 
 <style scoped>
 input:focus {
-  outline: none;
+    outline: none;
 }
+
 .inputBox {
-  background: white;
-  height: 50px;
-  line-height: 50px;
-  border-radius: 5px;
+    background: white;
+    height: 50px;
+    line-height: 50px;
+    border-radius: 5px;
 }
+
 .inputBox input {
-  border-style: none;
-  font-size: 0.9rem;
-  width: 80%;
+    border-style: none;
+    font-size: 0.9rem;
+    width: 80%;
 }
+
 .addContainer {
-  float: right;
-  background: linear-gradient(to right, #6478FB, #8763FB);
-  display: block;
-  width: 3rem;
-  border-radius: 0 5px 5px 0;
+    float: right;
+    background: linear-gradient(to right, #6478FB, #8763FB);
+    display: block;
+    width: 3rem;
+    border-radius: 0 5px 5px 0;
 }
+
 .addBtn {
-  color: white;
-  vertical-align: middle;
-}
-</style>
+    color: white;
+    vertical-align: middle;
+}</style>

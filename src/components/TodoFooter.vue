d1d6483850e014d50fac29d287d998f0b19591d1
mysoyul <vega2k_@naver.com>
2023-08-10 17:08:40 +0900

    footer update

--- src/components/TodoFooter.vue
+++ src/components/TodoFooter.vue
@@ -1,19 +1,32 @@
 <template>
-    <div>
-
+    <div class="clearAllContainer">
+        <span class="clearAllBtn" @click="clearTodo">Clear All</span>
     </div>
 </template>
 
 <script>
 export default {
-    setup () {
-        
-
-        return {}
+    setup() {
+        const clearTodo = () => {
+            localStorage.clear()
+        }
+        return { clearTodo }
     }
 }
 </script>
 
 <style scoped>
+.clearAllContainer {
+    width: 8.5rem;
+    height: 50px;
+    line-height: 50px;
+    background-color: white;
+    border-radius: 5px;
+    margin: 0 auto;
+}
 
+.clearAllBtn {
+    color: #e20303;
+    display: block;
+}
 </style>

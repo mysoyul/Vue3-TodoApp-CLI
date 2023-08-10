ef2cb016cc050d111e9dd9cb5681f25293637a71
mysoyul <vega2k_@naver.com>
2023-08-10 15:22:28 +0900

    TodoInput update

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <input type="text" :value="newTodoItem" @input="handleInput">
-        <button>추가</button>
+        <button @click="addTodo">추가</button>
     </div>
 </template>
 
@@ -10,13 +10,20 @@ import { ref, defineEmits } from 'vue';
 
 const newTodoItem = ref("")
 
+//input:todo 이름을 갖는 Event 정의
 const emit = defineEmits(["input:todo"])
 
 const handleInput = (event) => {
     const todoText = event.target.value
     if (!todoText) return
+    //Event 발생시킴
     emit("input:todo", todoText)
     newTodoItem.value = todoText
+}
+const addTodo = () => {
+    const todoItem = newTodoItem.value
+    localStorage.setItem(todoItem, todoItem)
+    newTodoItem.value = ""
 }
 
 </script>

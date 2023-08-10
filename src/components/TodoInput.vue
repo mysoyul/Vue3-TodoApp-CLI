a61f8695d4f79ebbcf2fdbcc3b131613d37a7ecc
mysoyul <vega2k_@naver.com>
2023-08-10 15:43:26 +0900

    TodoInput update

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -33,4 +33,30 @@ const clearInput = () => {
 
 </script>
 
-<style scoped></style>
+<style scoped>
+input:focus {
+  outline: none;
+}
+.inputBox {
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+  width: 80%;
+}
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #6478FB, #8763FB);
+  display: block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+}
+.addBtn {
+  color: white;
+  vertical-align: middle;
+}
+</style>

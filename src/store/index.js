1dce6f3b51effc1762969f38add50014ca75875c
mysoyul <vega2k_@naver.com>
2023-08-11 14:15:11 +0900

    moduleTodo add

--- src/store/index.js
+++ src/store/index.js
@@ -1,88 +1,10 @@
 import { createStore, createLogger } from "vuex"
-import http from "@/common/http-common"
-import axios from "axios"
+import { moduleTodo } from './modules/moduleTodo';
 
 export const store = createStore({
     plugins: process.env.NODE_ENV === 'development' ?
         [createLogger()] : [],
-    state: {
-        todoItems: []
-    },
-    actions: {
-        loadTodoItems({ commit }) {
-            http
-                .get('/todos')  //Promise
-                .then(res => res.data)
-                .then(items => {
-                    commit('setTodoItems', items)
-                })
-                .catch(error => {
-                    if (axios.isAxiosError(error)) {
-                        console.log(error?.response?.status +
-                            ' : ' + error.message)
-                    } else {
-                        console.error(error);
-                        throw new error;
-                    }
-                });
-        }, //loadTodoItems
-        removeTodo({ commit }, payload) {
-            http
-                .delete(`/todos/${payload.id}`)
-                .then(r => r.data)
-                .then(items => {
-                    commit('setTodoItems', items)
-                })
-        }, //removeTodo
-        addTodo({ commit }, payload) {
-            http
-                .post(`/todos`, payload)
-                .then(r => r.data)
-                .then(items => {
-                    commit('setTodoItems', items)
-                })
-        }, //addTodo
-        toggleTodo({ commit }, payload) {
-            http
-                .patch(`/todos/${payload.id}`, payload)
-                .then(r => r.data)
-                .then(items => {
-                    commit('setTodoItems', items)
-                })
-        }, //toggleTodo
-        clearTodo({ commit }) {
-            http
-                .delete('/todos')
-                .then(r => r.data)
-                .then(items => {
-                    commit('setTodoItems', items)
-                })
-        }
-    },
-    mutations: {
-        setTodoItems(state, items) {
-            state.todoItems = items;
-        },
-        addTodo(state, todoItem) {
-            const obj = { completed: false, item: todoItem };
-            localStorage.setItem(todoItem, JSON.stringify(obj));
-            state.todoItems.push(obj);
-        }, //addTodo
-        removeTodo(state, payload) {
-            const { todoItem, index } = payload
-            localStorage.removeItem(todoItem.item);
-            state.todoItems.splice(index, 1);
-        },
-        toggleTodo(state, payload) {
-            const { todoItem: { item, completed }, index } = payload
-            state.todoItems[index].completed = !completed
-            localStorage.removeItem(item);
-            localStorage.setItem(item, JSON.stringify(state.todoItems[index]));
-        },
-        clearTodo(state) {
-            localStorage.clear()
-            state.todoItems = []
-        }
-    },
-
+    modules: {
+        moduleTodo
+    }
 })
--- src/store/modules/moduleTodo.js
+++ src/store/modules/moduleTodo.js
@@ -6,7 +6,80 @@ export const moduleTodo = {
     state: () => ({
         todoItems: []
     }),
-    actions: {},
-    getters: {},
-    mutations: {},
+    actions: {
+        loadTodoItems({ commit }) {
+            http
+                .get('/todos')  //Promise
+                .then(res => res.data)
+                .then(items => {
+                    commit('setTodoItems', items)
+                })
+                .catch(error => {
+                    if (axios.isAxiosError(error)) {
+                        console.log(error?.response?.status +
+                            ' : ' + error.message)
+                    } else {
+                        console.error(error);
+                        throw new error;
+                    }
+                });
+        }, //loadTodoItems
+        removeTodo({ commit }, payload) {
+            http
+                .delete(`/todos/${payload.id}`)
+                .then(r => r.data)
+                .then(items => {
+                    commit('setTodoItems', items)
+                })
+        }, //removeTodo
+        addTodo({ commit }, payload) {
+            http
+                .post(`/todos`, payload)
+                .then(r => r.data)
+                .then(items => {
+                    commit('setTodoItems', items)
+                })
+        }, //addTodo
+        toggleTodo({ commit }, payload) {
+            http
+                .patch(`/todos/${payload.id}`, payload)
+                .then(r => r.data)
+                .then(items => {
+                    commit('setTodoItems', items)
+                })
+        }, //toggleTodo
+        clearTodo({ commit }) {
+            http
+                .delete('/todos')
+                .then(r => r.data)
+                .then(items => {
+                    commit('setTodoItems', items)
+                })
+        }
+    },
+    mutations: {
+        setTodoItems(state, items) {
+            state.todoItems = items;
+        },
+        addTodo(state, todoItem) {
+            const obj = { completed: false, item: todoItem };
+            localStorage.setItem(todoItem, JSON.stringify(obj));
+            state.todoItems.push(obj);
+        }, //addTodo
+        removeTodo(state, payload) {
+            const { todoItem, index } = payload
+            localStorage.removeItem(todoItem.item);
+            state.todoItems.splice(index, 1);
+        },
+        toggleTodo(state, payload) {
+            const { todoItem: { item, completed }, index } = payload
+            state.todoItems[index].completed = !completed
+            localStorage.removeItem(item);
+            localStorage.setItem(item, JSON.stringify(state.todoItems[index]));
+        },
+        clearTodo(state) {
+            localStorage.clear()
+            state.todoItems = []
+        }
+    },
 }

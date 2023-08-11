187554bd60d228caa22b8a03b1897592a1bb4164
mysoyul <vega2k_@naver.com>
2023-08-11 15:47:14 +0900

    PostDetail update

--- src/views/PostDetail.vue
+++ src/views/PostDetail.vue
@@ -1,13 +1,50 @@
 <template>
     <div>
-
+      <h2>View Post</h2>
+      <div v-if="loading">Loading...</div>
+      <div v-if="post">
+        <h3>[ID: {{ post.id }}]</h3>
+        <div>{{ post.text }}</div>
+          <button v-on:click="removePost(post.id)">
+          Delete
+        </button>
+      </div>
     </div>
-</template>
+  </template>
 
 <script setup>
+import { ref, onBeforeMount, computed, watch } from "vue";
+import { useRouter, useRoute } from "vue-router"
+import { useStore } from "vuex"
 
+const router = useRouter();
+const route = useRoute();
+const store = useStore();
+const loading = ref(false);
+const post = computed(() => store.state.modulePost.post);
+
+onBeforeMount(() => {
+  fetchData();
+});
+
+const removePost = (id) => {
+  console.log('-----------------removePost' + id)
+  store.dispatch("modulePost/removePost", id);
+  router.push("/posts");
+};
+const fetchData = () => {
+  loading.value = true;
+  if(route.params.id) {
+    store.dispatch("modulePost/loadPost", { id: +route.params.id }).then(() => {
+      loading.value = false;
+    });
+  }
+};
+
+watch(() => route.params.id, fetchData)
 </script>
-
 <style scoped>
-
-</style>
+button {
+  margin: 1rem 0;
+}
+</style>  

58ddd2400427d0d07a74ec33cca17a61655ee489
mysoyul <vega2k_@naver.com>
2023-08-11 15:33:06 +0900

    router post add

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/posts">Posts</router-link> |
+    <router-link to="/posts/new">New Post</router-link>
   </nav>
   <router-view/>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '@/views/HomeView.vue'
+import PostList from '@/views/PostList.vue'
+import PostNew from '@/views/PostNew.vue'
+import PostDetail from '@/views/PostDetail.vue'
 
 const routes = [
   {
@@ -14,6 +17,13 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/posts', component: PostList,
+    children: [
+      { path: ':id', component: PostDetail, name: 'post' },
+      { path: 'new', component: PostNew }
+    ]
   }
 ]
 

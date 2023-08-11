a1ba44fcdcf5d905f1c61d5a77165324c6a2b0d0
mysoyul <vega2k_@naver.com>
2023-08-11 15:39:39 +0900

    PostList update

--- src/views/PostList.vue
+++ src/views/PostList.vue
@@ -1,13 +1,41 @@
 <template>
     <div>
-
+        <h1>Posts</h1>
+        <div v-if="loading">Loading...</div>
+        <div v-for="post in posts" :key="post.id">
+            <router-link :to="{ name: 'post', params: { id: post.id } }">
+                [ID: {{ post.id }}] {{ summary(post.text) }}
+                <!-- </a>   -->
+            </router-link>
+        </div>
+        <router-view></router-view>
     </div>
 </template>
 
 <script setup>
+import { useStore } from "vuex"
+import { ref, onBeforeMount, computed } from "vue"
 
+const store = useStore()
+
+const loading = ref(false)
+const posts = computed(() => store.state.modulePost.posts)
+
+onBeforeMount(() => {
+    fetchData();
+});
+
+const fetchData = () => {
+    loading.value = true;
+    store.dispatch("modulePost/loadPosts").then(() => {
+        loading.value = false;
+    });
+};
+
+const summary = (val) => {
+    if (typeof val === "string") {
+        return val.substring(0, 20) + "...";
+    }
+    return val;
+};
 </script>
-
-<style scoped>
-
-</style>

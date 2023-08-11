5bb5610ed79a9539c296be7168cc42f6aa8ead7e
mysoyul <vega2k_@naver.com>
2023-08-11 14:10:37 +0900

    moduleTodo add

---
+++ src/store/modules/moduleTodo.js
@@ -0,0 +1,12 @@
+import axios from "axios"
+import http from "@/common/http-common"
+
+export const moduleTodo = {
+    namespaced: true,
+    state: () => ({
+        todoItems: []
+    }),
+    actions: {},
+    getters: {},
+    mutations: {},
+}
